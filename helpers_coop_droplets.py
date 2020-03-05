8e216df402d61db420e72a3944564543d3154941
dhdegroot <d.h.de.groot@vu.nl>
2020-03-05 23:15:38 +0100

    Working for one lambda

--- helpers_coop_droplets.py
+++ helpers_coop_droplets.py
@@ -1,6 +1,8 @@
 from itertools import product
 
-from numpy import array, log, exp
+import numpy as np
+import scipy
+from matplotlib import pyplot as plt
 from scipy.special import gammaln
 
 
@@ -14,11 +16,48 @@ def findTriplets(lst, key):
 def log_factorial(x):
     """Returns the logarithm of x!
     Also accepts lists and NumPy arrays in place of x."""
-    return gammaln(array(x) + 1)
+    return gammaln(np.array(x) + 1)
 
 
 def multinomial(xs, ps):
     n = sum(xs)
-    xs, ps = array(xs), array(ps)
-    result = log_factorial(n) - sum(log_factorial(xs)) + sum(xs * log(ps))
-    return exp(result)
+    xs, ps = np.array(xs), np.array(ps)
+    result = log_factorial(n) - sum(log_factorial(xs)) + sum(xs * np.log(ps))
+    return np.exp(result)
+
+
+def plot_pdf(val_freq_array, plot_type='Beautiful', xlabel='fraction cooperators', species_name='A'):
+    vals = val_freq_array[:, 0]
+    freqs = val_freq_array[:, 1]
+    mean = np.sum(vals * freqs) / np.sum(freqs)
+
+    bandwidth = 0.1 * vals.std() * vals.size ** (-1 / 5.)
+    support = np.linspace(min(vals) - bandwidth, max(vals) + bandwidth, 200)
+    supp_width = support[1] - support[0]
+
+    if plot_type == 'Beautiful':
+        kernels = []
+        for ind, val_i in enumerate(vals):
+            if ind % 1000 == 0:
+                print("Calculating mu dist: '{0}' out of '{1}'".format(ind, len(vals)))
+            kernel = scipy.stats.norm(val_i, bandwidth).pdf(support)
+            kernel = kernel * freqs[ind]
+            kernels.append(kernel)
+
+        density = np.sum(kernels, axis=0)
+        density /= scipy.integrate.trapz(density, support)
+    elif plot_type == "Fast":
+        density = np.zeros(len(support))
+        for ind, supp_i in enumerate(support):
+            supp_i_lb = supp_i - supp_width / 2
+            supp_i_ub = supp_i + supp_width / 2
+            density[ind] = np.sum(freqs[np.where((vals > supp_i_lb) & (vals <= supp_i_ub))])
+
+    if plot_type != 'None':
+        plt.figure()
+        plt.plot(support, density)
+        plt.xlabel(xlabel)
+        plt.ylabel('Probability distribution')
+        plt.title('PDF for ' + species_name)
+
+    return mean
--- main_coop_droplets.py
+++ main_coop_droplets.py
@@ -1,20 +1,28 @@
-import numpy as np
+import os
+
+import pandas as pd
 from scipy.stats import poisson
 
 from helpers_coop_droplets import *
 
 """Constants"""
-avg_nt = 8
+SAVE_FIGURES = True
+MAKE_PLOTS = False
+working_dir = os.getcwd()
+avg_nt = 1
 freqs = np.asarray([1 / 4, 1 / 4, 1 / 2])
 CC0 = 750
 CCC_ind = 100
 
 """First, find the range of the number of cells we expect in droplets"""
 nt_range = np.arange(poisson.ppf(0.0001, avg_nt), poisson.ppf(0.9999, avg_nt)).astype(int)
-p_species_sum = np.array([0., 0., 0.])
+droplet_df = pd.DataFrame(
+    columns=['na', 'nb', 'nc', 'prob_droplet', 'pa', 'pb', 'pc', 'coop_frac', 'mua', 'mub', 'muc'])
 
 """Then create all possible droplet compositions for one number of cells"""
 for nt in nt_range:
+    if nt % 10 == 0:
+        print("Calculated everything for droplets with less than '{0}' cells".format(nt))
     prob_nt = poisson.pmf(nt, avg_nt)
     droplets = findTriplets(np.arange(0, nt + 1), nt)
     for droplet in droplets:
@@ -23,13 +31,61 @@ for nt in nt_range:
 
         # Calculate prob of cell of species to be in this droplet (according to maintext-file)
         p_species = np.asarray([na, nb, nc]) / (freqs * avg_nt) * prob_droplet
-        p_species_sum += p_species
 
         # Find fraction of cooperator pairs
         denom = 2 * min(na, nb) + nc
         if denom == 0:
-            frac_coop = 0.0
+            coop_frac = 0.0
         else:
-            frac_coop = (2 * min(na, nb)) / denom
+            coop_frac = (2 * min(na, nb)) / denom
 
-print(p_species_sum)
+        # Find growth rates for all species in this droplet
+        end_ab = CC0 * coop_frac * (1 / 2) * (1 - (nc / nt)) if nt != 0 else 0.
+        mua = (1 / na) * end_ab if na != 0 else 0.
+        mub = (1 / nb) * end_ab if nb != 0 else 0.
+        muc = (1 / nc) * (CC0 * coop_frac + CCC_ind) * (nc / nt) if nc != 0 else 0.
+
+        droplet_df = droplet_df.append(
+            {'na': na, 'nb': nb, 'nc': nc, 'prob_droplet': prob_droplet, 'pa': p_species[0], 'pb': p_species[1],
+             'pc': p_species[2], 'coop_frac': coop_frac, 'mua': mua, 'mub': mub, 'muc': muc}, ignore_index=True)
+
+# Plot probability distribution for each species of being in a droplet with pairs of cooperators
+# Species A
+plot_type = 'Fast' if MAKE_PLOTS else 'None'
+val_freq_coop_frac_A = np.transpose(np.vstack((droplet_df.coop_frac.values, droplet_df.pa)))
+plot_pdf(val_freq_coop_frac_A, xlabel='fraction cooperators', species_name='A', plot_type=plot_type)
+if SAVE_FIGURES:
+    plt.savefig(os.path.join(working_dir, "results", "pdf_frac_coop_A.png"))
+
+# Species A growth rate
+val_freq_G_A = np.transpose(np.vstack((droplet_df.mua.values, droplet_df.pa)))
+avg_G_A = plot_pdf(val_freq_G_A, xlabel='growth factor', species_name='A', plot_type=plot_type)
+if SAVE_FIGURES:
+    plt.savefig(os.path.join(working_dir, "results", "pdf_G_A.png"))
+
+# Species B
+val_freq_coop_frac_B = np.transpose(np.vstack((droplet_df.coop_frac.values, droplet_df.pb)))
+plot_pdf(val_freq_coop_frac_B, xlabel='fraction cooperators', species_name='B', plot_type=plot_type)
+if SAVE_FIGURES:
+    plt.savefig(os.path.join(working_dir, "results", "pdf_frac_coop_B.png"))
+
+# Species B growth rate
+val_freq_G_B = np.transpose(np.vstack((droplet_df.mub.values, droplet_df.pb)))
+avg_G_B = plot_pdf(val_freq_G_B, xlabel='growth factor', species_name='B', plot_type=plot_type)
+if SAVE_FIGURES:
+    plt.savefig(os.path.join(working_dir, "results", "pdf_G_B.png"))
+
+# Species C
+val_freq_coop_frac_C = np.transpose(np.vstack((droplet_df.coop_frac.values, droplet_df.pc)))
+plot_pdf(val_freq_coop_frac_C, xlabel='fraction cooperators', species_name='C', plot_type=plot_type)
+if SAVE_FIGURES:
+    plt.savefig(os.path.join(working_dir, "results", "pdf_frac_coop_C.png"))
+
+# Species C
+val_freq_G_C = np.transpose(np.vstack((droplet_df.muc.values, droplet_df.pc)))
+avg_G_C = plot_pdf(val_freq_G_C, xlabel='growth_factors', species_name='C', plot_type=plot_type)
+if SAVE_FIGURES:
+    plt.savefig(os.path.join(working_dir, "results", "pdf_G_C.png"))
+
+print([avg_G_A, avg_G_B, avg_G_C])
+pass
